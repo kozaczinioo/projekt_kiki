87f3078b961cdaf5cd63c7573104694aacc8ad11
Kuba Prusakiewicz <jakubprusakiewicz@gmail.com>
2019-08-27 14:22:30 +0200

    tslint warnings fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule, MatMenuModule, MatCardModule } from "@angular/material";
+import { MatToolbarModule, MatMenuModule, MatCardModule } from '@angular/material';
 import { HeroComponent } from './hero/hero.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProductsComponent } from './products/products.component';
