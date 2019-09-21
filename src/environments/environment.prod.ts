26b5ed2907e912e83ca7f75ffff8224e0b3ddd1a
Kuba Prusakiewicz <jakubprusakiewicz@gmail.com>
2019-09-21 11:23:40 +0200

    lite version release

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+import { environment as common } from './environment';
+
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyArz6DIIgkmvegmiarKrik_9CpCBozVTis',
+    authDomain: 'kiki-c7b28.firebaseapp.com',
+    databaseURL: 'https://kiki-c7b28.firebaseio.com',
+    projectId: 'kiki-c7b28',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '<your-messaging-sender-id>'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: 'AIzaSyArz6DIIgkmvegmiarKrik_9CpCBozVTis',
     authDomain: 'kiki-c7b28.firebaseapp.com',
