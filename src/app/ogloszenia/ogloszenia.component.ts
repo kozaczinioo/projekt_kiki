6797d399c285d17586584b4b71823b2d21e4cafe
Kuba Prusakiewicz <jakubprusakiewicz@gmail.com>
2019-10-15 13:21:18 +0200

    feat items list display (no photos and filters yet)

--- src/app/ogloszenia/ogloszenia.component.ts
+++ src/app/ogloszenia/ogloszenia.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, AngularFireList, AngularFireObject} from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import { DbserviceService } from './../dbservice.service';
 
 
 @Component({
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './ogloszenia.component.html',
   styleUrls: ['./ogloszenia.component.scss']
 })
-export class OgloszeniaComponent implements OnInit {
+export class OgloszeniaComponent {
+
+  items: Observable<any[]>;
+  //items: AngularFireList<any[]>;
+  itemsRef: AngularFireList<any>;
 
 
-  ngOnInit(): void {
-
+  constructor(DbserviceService: DbserviceService, public db: AngularFireDatabase) {
+    this.itemsRef = DbserviceService.getItems();
+    this.items = this.itemsRef.valueChanges();
+    //console.log("bez" + this.items); //nie dziala
+    //this.items.subscribe(res => console.log("z"  + res)); //dziala
+    //this.items.valueChanges();
   }
 }
